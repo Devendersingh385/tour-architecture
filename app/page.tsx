1b4d2b11e8029330a594d0b39cc7b2e93a8f1404
devender singh <devendersingh385@gmail.com>
2024-05-10 18:57:51 +0530

    update chnage.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+      <div className="bg-[#5452f5] py-5">
+        <div className="grid text-center lg:mb-0 lg:w-full lg:max-w-screen-xl lg:grid-cols-2 lg:text-left">
+          <div className="">
+            <h1>Adventure Awaits: Gear Up with Tour Architecture!</h1>
+            <p>Gear Up for Adventure with Tour Architecture: Rent or Buy Top-Quality Hiking Accessories Today!"</p>
+          </div>
+          <div className="">
+            <h1>Adventure Awaits: Gear Up with Tour Architecture!</h1>
+          </div>
+        </div>
+      </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
