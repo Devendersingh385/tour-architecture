c94b510913d3d221583a1359dae00c494d981cbf
Devender singh <devendersingh385@gmail.com>
2024-05-23 23:55:12 +0530

    update page.

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
           </div>
           <div className="grid text-center gap-6 lg:mb-0 lg:max-w-screen-xl mx-auto lg:grid-cols-4 lg:text-left">
             <div className="">
-              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full">
                 <a href="#">
                   <Image
                     className="rounded-t-lg"
@@ -45,7 +45,7 @@ export default function Home() {
                 </a>
                 <div className="p-5">
                   <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white min-h-[64px]">
                       Camping - 2 Person
                     </h5>
                   </a>
@@ -65,10 +65,281 @@ export default function Home() {
                 </a>
                 <div className="p-5">
                   <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white min-h-[64px]">
                       Camping - 3 Person
                     </h5>
                   </a>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <Image
+                    className="rounded-t-lg"
+                    src="/sleeping-bag.webp"
+                    alt=""
+                    width={500}
+                    height={300}
+                  />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white min-h-[64px]">
+                      Sleeping Bag 0-5 degree
+                    </h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <Image
+                    className="rounded-t-lg"
+                    src="/Sleeping-bag-simple.webp"
+                    alt=""
+                    width={500}
+                    height={300}
+                  />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white min-h-[64px]">
+                      Camping Sleeping Bag10°
+                    </h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <Image
+                    className="rounded-t-lg"
+                    src="/gas.webp"
+                    alt=""
+                    width={500}
+                    height={300}
+                  />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white min-h-[64px]">
+                      Camping Stove (Portable)
+                    </h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <Image
+                    className="rounded-t-lg"
+                    src="/Butane-Canister.webp"
+                    alt=""
+                    width={500}
+                    height={300}
+                  />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white min-h-[64px]">
+                      Butane Canister
+                    </h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <Image
+                    className="rounded-t-lg"
+                    src="/Battery-Head-Torch.webp"
+                    alt=""
+                    width={500}
+                    height={300}
+                  />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white min-h-[64px]">
+                      FORCLAZ Battery Head Torch
+                    </h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <Image
+                    className="rounded-t-lg"
+                    src="/tourch.webp"
+                    alt=""
+                    width={500}
+                    height={300}
+                  />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white min-h-[64px]">
+                      GENERIC LED TOURCH
+                    </h5>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" py-10 relative">
+            <div className="grid text-center gap-4 lg:mb-0 lg:max-w-screen-xl mx-auto lg:grid-cols-1 lg:text-left">
+              <h2 className="text-[42px] font-bold">Why Choose Us</h2>
+            </div>
+            <div className="grid text-center gap-6 lg:mb-0 lg:max-w-screen-xl mx-auto lg:grid-cols-3 lg:text-left">
+              <div className="">
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-[32px] h-full">
+                  <a href="#">
+                    <img
+                      className="rounded-t-lg"
+                      src="/quality-assurance.png"
+                      alt=""
+                      width={100}
+                      height={100}
+                    />
+                  </a>
+                  <div className="p-5">
+                    <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Quality Products
+                      </h5>
+                    </a>
+                    <p>
+                      We do a strict quality-check for every product, as
+                      ensuring quality product matters to us.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-[32px] h-full">
+                  <a href="#">
+                    <img
+                      className="rounded-t-lg"
+                      src="/shopping-bag.png"
+                      alt=""
+                      width={100}
+                      height={100}
+                    />
+                  </a>
+                  <div className="p-5">
+                    <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        One-stop rental solution
+                      </h5>
+                    </a>
+                    <p>
+                      All equipment rental solution is under one roof on VikNick
+                      Services.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-[32px] h-full">
+                  <a href="#">
+                    <img
+                      className="rounded-t-lg"
+                      src="/shopping-bag.png"
+                      alt=""
+                      width={100}
+                      height={100}
+                    />
+                  </a>
+                  <div className="p-5">
+                    <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        One-stop rental solution
+                      </h5>
+                    </a>
+                    <p>
+                      All equipment rental solution is under one roof on VikNick
+                      Services.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-[32px] h-full">
+                  <a href="#" className="mb-4 block">
+                    <img
+                      className="rounded-t-lg"
+                      src="/profit-growth.png"
+                      alt=""
+                      width={80}
+                      height={80}
+                    />
+                  </a>
+                  <div className="">
+                    <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Economical solutions
+                      </h5>
+                    </a>
+                    <p>
+                      All equipment rental solution is under one roof on VikNick
+                      Services.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-[32px] h-full">
+                  <a href="#" className="mb-4 block">
+                    <img
+                      className="rounded-t-lg"
+                      src="/customer-service.png"
+                      alt=""
+                      width={80}
+                      height={80}
+                    />
+                  </a>
+                  <div className="">
+                    <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Customer support
+                      </h5>
+                    </a>
+                    <p>
+                      All equipment rental solution is under one roof on VikNick
+                      Services.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-[32px] h-full">
+                  <a href="#" className="mb-4 block">
+                    <img
+                      className="rounded-t-lg"
+                      src="/traveling.png"
+                      alt=""
+                      width={80}
+                      height={80}
+                    />
+                  </a>
+                  <div className="">
+                    <a href="#">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        Door to door delivery
+                      </h5>
+                    </a>
+                    <p>
+                      All equipment rental solution is under one roof on VikNick
+                      Services.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
